94854d082f77968d8512bcafafbaacaeecf4c7e0
ericcacordier <ericcacordier@hotmail.com>
2015-03-19 14:49:24 -0500

    changes
    changes

---
+++ alert.js
@@ -0,0 +1,12 @@
+  $(document).ready(function () {
+
+        $("#btnSave").click(
+            function () {
+                AlertSave();
+            }            
+        );
+    });
+
+function AlertSave() {
+   alert("Bobby is 2 years old");
+}
